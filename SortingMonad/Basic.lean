83f29cd763cc7f5f0ad7f1622b8afd17e7622a49
Christina A. Upshaw <chrisaupshaw@gmail.com>
2024-12-31 01:48:04 -0800

    WIP

--- SortingMonad/Basic.lean
+++ SortingMonad/Basic.lean
@@ -82,22 +82,15 @@ class MonadSortLawful extends MonadCmpLawful F where
   swap_idem (i j) : no_change (do {swap i j; swap i j})
   swap_cmp_swap (i j) : (do {swap i j; let b <- (j ≤? i : F Bool); swap i j; pure b}) ≃ i ≤? j
 
-structure SortingLog (n : ℕ) : Type where
-  cmp_at : List (Fin n × Fin n)
-  swap : List (Fin n × Fin n)
+structure SortingLogItem (n : ℕ) where
+  isSwap : Bool
+  left : Fin n
+  right : Fin n
 
-instance {n} : EmptyCollection (SortingLog n) where
-  emptyCollection := {cmp_at := ∅, swap := ∅}
+abbrev SortingLog n := List (SortingLogItem n)
 
-instance {n} : Append (SortingLog n) where
-  append old new := ⟨new.1 ++ old.1, new.2 ++ old.2⟩ -- put the new one on front
-
-def SortingMonad (item : Type) (size : ℕ) (ret : Type) : Type :=
+abbrev SortingMonad (item : Type) (size : ℕ) (ret : Type) : Type :=
   ReaderT (Fin size → item) (StateT (Fin size ≃ Fin size) (Writer (SortingLog size))) ret
-
-def test {m} [Monad m] (f : α → β → β) : ReaderT α (StateT β m) Unit := do
-  let a <- read
-  modify (f a)
 
 variable {size : ℕ} {item : Type}
 
@@ -112,12 +105,48 @@ instance : Monad (SortingMonad item size) :=
       inferInstance
     inst
 
-instance [Monad (Writer ω)] : LawfulMonad (Writer ω) where
-  map_const := funext _
+instance (ℓ : Type*) : LawfulMonad (Writer (List ℓ)) where
+  map_const {α β} := by
+    funext a mb
+    let (b, w) := mb
+    simp [Functor.mapConst, WriterT.mk, Id.instMonad, Functor.map]
   id_map a :=
     match a with
-    | (x, w) => _
-  
+    | (x, w) => by
+      simp [Functor.map, WriterT.mk]
+  seqLeft_eq := by
+    intros α β x y
+    let (x₁, x₂) := x
+    let (y₁, y₂) := y
+    simp [Functor.map, SeqLeft.seqLeft, Id.instMonad, WriterT.mk, Seq.seq]
+  seqRight_eq := by
+    intros α β x y
+    let (x₁, x₂) := x
+    let (y₁, y₂) := y
+    simp [Functor.map, SeqRight.seqRight, Id.instMonad, WriterT.mk, Seq.seq]
+  pure_seq := by
+    intros α β f x
+    let (x₁, x₂) := x
+    simp [Functor.map, Pure.pure, Id.instMonad, WriterT.mk, Seq.seq]
+  bind_pure_comp := by
+    intros α β f x
+    let (x₁, x₂) := x
+    simp [bind, pure, Functor.map]
+  bind_map := by
+    intros α β f x
+    let (x₁, x₂) := x
+    let (f₁, f₂) := f
+    simp [bind, pure, Functor.map, WriterT.mk, Seq.seq, Id.instMonad]
+  pure_bind := by
+    intros α β x f
+    simp [bind, pure, Functor.map, WriterT.mk, Seq.seq, Id.instMonad]
+    rfl
+  bind_assoc := by
+    intros α β γ x f g
+    let (x₁, x₂) := x
+    simp [bind, WriterT.mk]
+    let (fx₁, fx₂) := f x₁
+    simp
 
 def SortingMonad.cmp_at [LinearOrder item] (i j : Fin size) : SortingMonad item size Bool :=
   do
@@ -141,12 +170,47 @@ instance [Inhabited item] [LinearOrder item] : MonadSort $ SortingMonad item siz
     apply h
     apply default
 
+theorem instance_helper {item n} : ((fun _ ↦ True) <$> (get : SortingMonad item n _) = pure True) := by
+      simp_rw [Functor.map, get, getThe]
+      simp [MonadStateOf.get, liftM, monadLift, MonadLift.monadLift, pure]
+      funext env
+      simp_rw [ReaderT.pure, pure]
+      funext state
+      simp [StateT.pure, StateT.map, StateT.get]
+
+theorem instance_helper2 {item n} : ((fun _ ↦ True) <$> (read : SortingMonad item n _) = pure True) := by
+      simp_rw [Functor.map, read, readThe]
+      simp [MonadReaderOf.read, ReaderT.read]
+      funext env state
+      have : ∀ {α β s m} [Monad m] (f : α → β) (x : StateT s m α), StateT.map f x = f <$> x := λ a b => by
+        simp [Functor.map]
+      rw [this, map_pure]
+      simp [pure, StateT.pure, ReaderT.pure]
+
+theorem SortingMonad.map_run_result {item n} (f : α → β) (m : SortingMonad item n α) (data) :
+  (f <$> m).run data = (f (m.run data).1, (m.run data).2) := by
+  simp_rw [SortingMonad.run, ReaderT.run, Functor.map]
+  have : ∀ {σ M} [Monad M] (s : StateT σ M α) (f : α → β), StateT.map f s = f <$> s := by
+    intros σ M inst s f
+    rfl
+  rw [this, StateT.run_map]
+  rw [StateT.run]
+  let ((a, result), log) := (m data 1)
+  simp [Functor.map, WriterT.mk]
+
 instance [Inhabited item] [LinearOrder item] : MonadSortLawful $ SortingMonad item size where
   cmp_at_refl := by
     intros mi data
-    rw [LawfulApplicative.seq_pure]
+    simp [seq_pure, MonadSort.cmp_at, SortingMonad.cmp_at, Bind.bindLeft, instance_helper]
+    simp [instance_helper2]
+    simp [SortingMonad.map_run_result]
+  cmp_at_trans := by
+    intros i j k data
+    simp [seq_pure, MonadSort.cmp_at, SortingMonad.cmp_at, Bind.bindLeft, instance_helper]
+    simp [read, readThe, MonadReaderOf.read, ReaderT.read]
+    simp_rw [get, getThe, MonadStateOf.get, liftM, monadLift, MonadLift.monadLift]
+    
 
-  cmp_at_trans := _
   cmp_idem := _
   swap_rfl := _
   swap_symm := _
